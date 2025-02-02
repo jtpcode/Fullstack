c94d77967a12cc1d102782852f05ff9cbecc73b5
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-02-02 10:55:08 +0200

    E2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,74 +1,110 @@
 import { useState } from 'react'
 
-const Phonebook = ({ newName, handleNameChange, addName }) => {
+const Filter = ({ newFilter, handleFilterChange }) => {
   return (
-    <form onSubmit={addName}>
+    <div>
+      Filter shown numbers with: 
+      <input
+        value={newFilter}
+        onChange={handleFilterChange}
+      />
+    </div>
+  )
+}
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => {
+  return (
+    <form onSubmit={addPerson}>
       <div>
-        name: <input 
-                value={newName}
-                onChange={handleNameChange}
-              />
+        Name: 
+        <input 
+          value={newName}
+          onChange={handleNameChange}
+        />
       </div>
       <div>
-        <button type="submit">add</button>
+        Number: 
+        <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">Add</button>
       </div>
     </form>
   )
 }
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, newFilter }) => {
+  const filtered_persons = persons.filter(person =>
+    person.name.toLowerCase().includes(newFilter.toLowerCase())
+  )
+
   return (
     <div>
-      {persons.map(person => 
+      {filtered_persons.map(person => 
         <Person key={person.name} person={person} />
       )}
     </div>
   )
 }
 
-const Person = ({ person }) => <p>{person.name}</p>
+const Person = ({ person }) => <p>{person.name}: {person.number}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName
+    const personObject = {
+      name: newName,
+      number: newNumber
     }
 
-    const nameExists = persons.some(person => person.name === newName)
+    const personExists = persons.some(person => person.name === newName)
 
-    {nameExists ? alert(`Name ${newName} already exists.`) : setPersons(persons.concat(nameObject))}
+    {personExists ? alert(`Name ${newName} already exists.`) : setPersons(persons.concat(personObject))}
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Phonebook
-        newName={newName}
-        handleNameChange={handleNameChange}
-        addName={addName}
+      <Filter newFilter={newFilter}
+              handleFilterChange={handleFilterChange}
       />
 
-      {/* <Filter ... />
-
-      <h3>Add a new</h3>
-
-      <PersonForm 
-        ...
-      /> */}
+      <h3>Add a new person</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
 
       <h3>Numbers</h3>
-      <Persons persons={persons}/>
+      <Persons persons={persons} newFilter={newFilter}/>
     </div>
     
   )
