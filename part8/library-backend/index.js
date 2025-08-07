17c3b1935847fcf61eb157d907105898862c62c1
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-08-07 14:37:56 +0300

    E8.3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -101,6 +101,7 @@ const typeDefs = `
   type Author {
     name: String!
     born: Int
+    bookCount: Int!
     id: ID!
   }
 
@@ -115,13 +116,22 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter((book) => book.author === root.name).length
+    }
   }
 }
 
