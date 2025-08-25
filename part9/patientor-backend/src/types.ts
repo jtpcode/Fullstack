3bb260809261ef7c0e9aa36598f7e9359c593f6e
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-08-25 12:16:00 +0300

    E9.24

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -62,7 +62,7 @@ export interface Patient {
   ssn: string;
   gender: Gender;
   occupation: string;
-  entries: Entry[]
+  entries: Entry[];
 }
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
--- part9/patientor-frontend/src/components/PatientPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -40,15 +40,15 @@ const PatientPage = () => {
   if (!patient) {
     return (
       <Box>
-        <Typography variant="h6">Loading patient data...</Typography>
+        <Typography variant='h6'>Loading patient data...</Typography>
       </Box>
     );
   }
 
   return (
     <Box>
-      <Box display="flex" alignItems="center" gap={1} mt={3} mb={3}>
-        <Typography variant="h4">
+      <Box display='flex' alignItems='center' gap={1} mt={3} mb={3}>
+        <Typography variant='h4'>
           {patient.name}
         </Typography>
         {getGenderIcon(patient.gender)}
@@ -56,15 +56,37 @@ const PatientPage = () => {
 
       <Box>        
         <Box mb={2}>
-          <Typography variant="body1">
-            <strong>SSN:</strong> {patient.ssn || '-'}
+          <Typography variant='body1'>
+            SSN: {patient.ssn || '-'}
+            <br />
+            Occupation: {patient.occupation}
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box>
+        <Box>
+          <Typography variant='h5'>
+            Entries
           </Typography>
         </Box>
 
-        <Box mb={2}>
-          <Typography variant="body1">
-            <strong>Occupation:</strong> {patient.occupation}
-          </Typography>
+        <Box>
+          <Box>
+            <Typography variant='body1'>
+              {patient.entries[0].date}:{' '}
+              <em>{patient.entries[0].description}</em>
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant='body1' component='div'>
+              <ul>
+                {patient.entries[0].diagnosisCodes?.map(code => (
+                  <li key={code}>{code}</li>
+                ))}
+              </ul>
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
